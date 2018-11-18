753d45a9cbea68a7f8468f0d209e013495f8cc73
BDankovych <bohdan.dankovych@gmail.com>
2018-11-18 15:28:11 +0200

    Init commit

---
+++ ForecastApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForecastApp
+//
+//  Created by Bohdan Dankovych on 11/18/18.
+//  Copyright © 2018 Bohdan Dankovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
